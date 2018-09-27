1ea287952500d6513775475a9e46798ce0653073
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-09-27 20:24:21 +0530

    Initial auth0 setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import PendingTasks from './pendingTasks';
 import CompletedTasks from './completedTasks';
 import AddTask from './addTask';
+import Auth from './Auth/Auth.js';
 
 const client = new ApolloClient({
   uri: "https://hasura-pradeep.herokuapp.com/v1alpha1/graphql"
 });
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>To Do App: Powered by Hasura and Apollo</h2>
-      <hr/>
-    </div>
-    <div>
-      <h3>Pending Tasks</h3>
-    </div>
-    <AddTask />
-    <PendingTasks />
-    <div>
-      <h3>Tasks Completed </h3>
-    </div>
-    <CompletedTasks />
-  </ApolloProvider>
-);
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <h2>To Do App: Powered by Hasura and Apollo <span>
+          {
+            !isAuthenticated() && (
+                <button
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.login.bind(this)}
+                >
+                  Log In
+                </button>
+              )
+          }
+          {
+            isAuthenticated() && (
+                <button
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.logout.bind(this)}
+                >
+                  Log Out
+                </button>
+              )
+          }
+          </span></h2>
+          <hr/>
+        </div>
+        <div>
+          <h3>Pending Tasks</h3>
+        </div>
+        <AddTask />
+        <PendingTasks />
+        <div>
+          <h3>Tasks Completed </h3>
+        </div>
+        <CompletedTasks />
+      </ApolloProvider>
+    )
+  }
+}
+
+const auth = new Auth();
+auth.login();
 
 export default App;
